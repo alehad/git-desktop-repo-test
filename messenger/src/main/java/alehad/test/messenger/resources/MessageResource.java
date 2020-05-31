bf94eb4e1c70e836e0ff0af51e13d2dcad482425
alehad <64356216+alehad@users.noreply.github.com>
2020-05-31 13:26:11 +0100

    added MessageResource class, but server is not happy!

---
+++ messenger/src/main/java/alehad/test/messenger/resources/MessageResource.java
@@ -0,0 +1,19 @@
+package alehad.test.messenger.resources;
+
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+
+@Path("/messages")
+public class MessageResource {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessage()
+	{
+		return "Hello there!";
+	}
+}
